d665126e8c694c0f6a60c342caad063b55d3bf07
buller <lizometrix@gmail.com>
2022-07-22 20:20:52 +0300

    Changed delay

--- SimpleMQTTClient/SimpleMQTTClient.ino
+++ SimpleMQTTClient/SimpleMQTTClient.ino
@@ -4,16 +4,26 @@
 #include <BME280I2C.h>
 #include <Wire.h>
 #include <BH1750.h>
+#include <EEPROM.h>
 
 
 #define SERIAL_BAUD 115200
-#define SDA_PIN D2
-#define SCL_PIN D1
+#define SDA_PIN 13
+#define SCL_PIN 12
 #define LUX_WAIT_DELAY_MS 100
 #define MQTT_LOOP_NUM 10
 #define MQTT_LOOP_DELAY_MS 100
+#define DEBUG
 
-long SLEEPTIME = 1 * 5e6; // 15 min
+static const long SLEEP_MULT = 60e6;
+static const int DEFAULT_SLEEP_MIN = 1;
+static const int EEPROM_ADDR = 0;
+#define SLEEP_MIN(m) (m * SLEEP_MULT)
+
+typedef struct {
+  long sleeptime;
+} config_t;
+config_t config;
 
 // ###################  Sensors related 
 BH1750 lightMeter;
@@ -39,14 +49,15 @@ typedef struct {
 const char* WLAN_SSID = "MASLO";
 const char* WLAN_PASSWD = "Moroz1vo";
 const char* MQTT_BROKER_IP = "192.168.1.106";
+const int MQTT_PORT = 1883;
 const char* MQTT_USERNAME = "MeteoProbeOut";
 const char* MQTT_PASSWD = "nzl98m9";
 const char* MQTT_CLIENT_ID = "meteo_probe1";
-const char* MQTT_TEMP_TOPIC = "home/sensors/out/temperature";
+const char* MQTT_TEMP_TOPIC =  "home/sensors/out/temperature";
 const char* MQTT_PRESS_TOPIC = "home/sensors/out/pressure";
-const char* MQTT_HUM_TOPIC = "home/sensors/out/humidity";
-const char* MQTT_LUX_TOPIC = "home/sensors/out/lux";
-const char* MQTT_CFG_TOPIC = "home/sensors/out/cfg/reload";
+const char* MQTT_HUM_TOPIC =   "home/sensors/out/humidity";
+const char* MQTT_LUX_TOPIC =   "home/sensors/out/lux";
+const char* MQTT_CFG_TOPIC =   "home/sensors/out/cfg/period";
 WiFiClient espClient;
 PubSubClient mqtt_client(espClient);
 // ###################################
@@ -54,7 +65,9 @@ PubSubClient mqtt_client(espClient);
 void setup()
 {
   environ_data_t environ_data;
-
+  delay(50);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
   modulesSetup();
 
   get_thp_data(&environ_data);
@@ -62,7 +75,12 @@ void setup()
 
   waitForWifiConnection();
 
-  mqtt_client.setServer(MQTT_BROKER_IP, 1883);
+//  while(1) {
+//    Serial.println(WiFi.RSSI());
+//    delay(200);
+//  }
+
+  mqtt_client.setServer(MQTT_BROKER_IP, MQTT_PORT);
   mqtt_client.setCallback(callback);
   if (!mqtt_client.connected()) {
     reconnect();
@@ -78,11 +96,16 @@ void setup()
 
   WiFi.disconnect(true);
   delay(1);
-  ESP.deepSleep( SLEEPTIME, WAKE_RF_DISABLED );
+  Serial.println("Going to sleep");
+  digitalWrite(LED_BUILTIN, HIGH);
+  ESP.deepSleep(SLEEP_MIN(5), WAKE_RF_DISABLED );
 }
 
 void modulesSetup(){
+#ifdef DEBUG
   Serial.begin(SERIAL_BAUD);
+#endif
+  Serial.println("Woken up!");
   WiFi.mode( WIFI_OFF );
   WiFi.forceSleepBegin();
   delay(1);
@@ -95,15 +118,21 @@ void modulesSetup(){
   settings.tempOSR = BME280::OSR_X4;
   bme.setSettings(settings);
   lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
+
+  EEPROM.begin(sizeof(config));
+  EEPROM.get(EEPROM_ADDR, config);
+  if (!config.sleeptime) config.sleeptime = DEFAULT_SLEEP_MIN;
 }
 
 void sendMQTTmessage(environ_data_t *environ_data)
 {
-  Serial.printf("Message: temp %f, pres %f, hum %f, lux %f\n", environ_data->temp, environ_data->pres, environ_data->hum, environ_data->lux);
-  mqtt_client.publish(MQTT_TEMP_TOPIC, String(environ_data->temp, 4).c_str(), false);
-  mqtt_client.publish(MQTT_PRESS_TOPIC, String(environ_data->pres, 4).c_str(), false);
-  mqtt_client.publish(MQTT_HUM_TOPIC, String(environ_data->hum, 4).c_str(), false);
-  mqtt_client.publish(MQTT_LUX_TOPIC, String(environ_data->lux, 4).c_str(), false);
+#ifdef DEBUG
+  Serial.printf("Message: temp %.02f, pres %.02f, hum %.02f, lux %.02f\n", environ_data->temp, environ_data->pres, environ_data->hum, environ_data->lux);
+#endif
+  mqtt_client.publish(MQTT_TEMP_TOPIC, String(environ_data->temp, 2).c_str(), false);
+  mqtt_client.publish(MQTT_PRESS_TOPIC, String(environ_data->pres, 2).c_str(), false);
+  mqtt_client.publish(MQTT_HUM_TOPIC, String(environ_data->hum, 2).c_str(), false);
+  mqtt_client.publish(MQTT_LUX_TOPIC, String(environ_data->lux, 2).c_str(), false);
   /* Close MQTT client cleanly */
   mqtt_client.disconnect();
 }
@@ -114,18 +143,16 @@ void reconnect() {
     if (mqtt_client.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWD))
     {
       Serial.print("Connected");
-    mqtt_client.subscribe(MQTT_CFG_TOPIC);
+      mqtt_client.subscribe(MQTT_CFG_TOPIC);
     } else {
       Serial.print("failed, rc= ");
       Serial.print(mqtt_client.state());
       delay(1000);
     }
-
   }
 }
 
 void waitForWifiConnection(){
-
   WiFi.forceSleepWake();
   delay(1);
   WiFi.mode( WIFI_STA );
@@ -150,18 +177,20 @@ void waitForWifiConnection(){
       WiFi.disconnect( true );
       delay( 1 );
       WiFi.mode( WIFI_OFF );
-      ESP.deepSleep( SLEEPTIME, WAKE_RF_DISABLED );
+      Serial.println("Going to sleep!");
+      ESP.deepSleep( SLEEP_MULT * 1, WAKE_RF_DISABLED );
       return; // Not expecting this to be called, the previous call will never return.
     }
     delay( 50 );
     wifiStatus = WiFi.status();
   }
+  Serial.println("WIFI connected!");
 }
 
 void get_thp_data(environ_data_t *environ_data)
 {
    BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
-   BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+   BME280::PresUnit presUnit(BME280::PresUnit_hPa);
    bme.read(environ_data->pres, environ_data->temp, environ_data->hum, tempUnit, presUnit);
 }
 
@@ -175,15 +204,20 @@ void get_lux_data(environ_data_t *environ_data)
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
- 
+ #ifdef DEBUG
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
- 
   Serial.print("Message:");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
- 
+#endif
+  if (!length) return;
+  EEPROM.begin(sizeof(config));
+  config.sleeptime = payload[0];
+  EEPROM.put(EEPROM_ADDR, config);
+  EEPROM.commit();
+  EEPROM.end();
 }
 
 void loop()
